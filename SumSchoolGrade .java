fe637759b105d48e7b8a029b5a0962c6b8133242
KARINA ALMEIDA <166922968+Kaah92@users.noreply.github.com>
2024-09-04 11:04:19 -0300

    Update SumSchoolGrade .java

--- SumSchoolGrade .java
+++ SumSchoolGrade .java
@@ -1,8 +1,3 @@
-/*
-Q10) Faça um programa que possua o método "mediaDeNotas()" que imprima a média das notas do vetor (II).
-	double notas[] = {7.9, 6.4, 3.8, 9.7, 5.6, 10.0, 7.2, 6.5, 6.8, 8.7, 4.3, 7.3, 8.0, 7.0};
-*/
-
 class SumSchoolGrade  {
 
 	static double schoolGrade[] = {7.9, 6.4, 3.8, 9.7, 5.6, 10.0, 7.2, 6.5, 6.8, 8.7, 4.3, 7.3, 8.0, 7.0};
